76decd8adb8b2c8121c856cf0ba27f1d89cfc1cc
Oleksandr Maniukhin <om7755@g.rit.edu>
2025-03-24 19:41:51 -0400

    Service with reg key parameters

--- Client/BITS_TEST/BITS_TEST/client.cpp
+++ Client/BITS_TEST/BITS_TEST/client.cpp
@@ -2,49 +2,64 @@
 #include "file_operations.h"
 #include <windows.h>
 
-//SERVICE_STATUS ServiceStatus;
-//SERVICE_STATUS_HANDLE hStatusHandle;
-//
-//void WINAPI ServiceCtrlHandler(DWORD ctrlCode)
-//{
-//    switch (ctrlCode) {
-//    case SERVICE_CONTROL_STOP:
-//        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
-//        SetServiceStatus(hStatusHandle, &ServiceStatus);
-//        break;
-//    default:
-//        break;
-//    }
-//}
+SERVICE_STATUS ServiceStatus;
+SERVICE_STATUS_HANDLE hStatusHandle;
 
-// Function to generate a random filename like 'ksfjs32dj4545kf.txt'
+std::wstring remoteUrl;
+std::wstring localDir;
 
+void WINAPI ServiceCtrlHandler(DWORD ctrlCode)
+{
+    switch (ctrlCode) {
+    case SERVICE_CONTROL_STOP:
+        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        SetServiceStatus(hStatusHandle, &ServiceStatus);
+        break;
+    default:
+        break;
+    }
+}
 
-//void WINAPI ServiceMain(DWORD argc, LPSTR* argv)
-//{
-//    hStatusHandle = RegisterServiceCtrlHandler(L"BITSHelper", ServiceCtrlHandler);
-//    if (hStatusHandle == NULL) {
-//        return;
-//    }
-//
-//    ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
-//    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
-//    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
-//    SetServiceStatus(hStatusHandle, &ServiceStatus);
-//
-//    // Start your executable logic here
-//    
-//}
-
-int wmain(int argc, wchar_t* argv[]) {
-    // Check if the user provided the remote URL and local directory as parameters
-    if (argc < 3) {
-        wprintf(L"Usage: <program> <remoteUrl> <localDirectory>\n");
-        return 1;  // Return an error if the required parameters are missing
+void WINAPI ServiceMain(DWORD argc, LPSTR* argv)
+{
+    hStatusHandle = RegisterServiceCtrlHandler(L"BITSHelper", ServiceCtrlHandler);
+    if (hStatusHandle == NULL) {
+        return;
     }
 
-    std::wstring remoteUrl = argv[1];  // Use the first command-line argument as the remote URL
-    std::wstring localDir = argv[2];   // Use the second command-line argument as the local directory
+    ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+    SetServiceStatus(hStatusHandle, &ServiceStatus);
+
+    // Read parameters from the registry
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Services\\bitHelp", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        DWORD dwSize;
+        wchar_t szRemoteUrl[256], szLocalDir[256];
+
+        // Get remote URL
+        dwSize = sizeof(szRemoteUrl);
+        if (RegQueryValueEx(hKey, L"RemoteUrl", NULL, NULL, (LPBYTE)szRemoteUrl, &dwSize) == ERROR_SUCCESS) {
+            remoteUrl = szRemoteUrl;  // Directly assign to remoteUrl
+            wprintf(L"Remote URL: %ls\n", remoteUrl.c_str());
+        }
+
+        // Get local directory
+        dwSize = sizeof(szLocalDir);
+        if (RegQueryValueEx(hKey, L"LocalDir", NULL, NULL, (LPBYTE)szLocalDir, &dwSize) == ERROR_SUCCESS) {
+            localDir = szLocalDir;  // Directly assign to localDir
+            wprintf(L"Local Directory: %ls\n", localDir.c_str());
+        }
+
+        RegCloseKey(hKey);
+    }
+
+    // Check if the parameters were successfully retrieved
+    if (remoteUrl.empty() || localDir.empty()) {
+        wprintf(L"Error: Missing parameters from registry.\n");
+        return;
+    }
 
     // Ensure the local directory path ends with a backslash (if not already)
     if (localDir.back() != L'\\') {
@@ -53,16 +68,24 @@ int wmain(int argc, wchar_t* argv[]) {
 
     std::wstring localFile = localDir + GenerateRandomFilename();  // Append random filename to the directory path
 
-    wprintf(L"Using URL: %s\n", remoteUrl.c_str());
-    wprintf(L"Generated local file: %s\n", localFile.c_str());
+    wprintf(L"Using URL: %ls\n", remoteUrl.c_str());
+    wprintf(L"Generated local file: %ls\n", localFile.c_str());
 
-    // Simulate a repeated download every minute
-    while (true) {
+    // Start your executable logic here
+    while (ServiceStatus.dwCurrentState == SERVICE_RUNNING) {
         CoInitialize(nullptr);
         DownloadFile(remoteUrl, localFile);  // Download the file with the generated filename
         CoUninitialize();
         Sleep(60000);  // Wait for 60 seconds before downloading again
     }
+}
 
+int wmain(int argc, wchar_t* argv[]) {
+    SERVICE_TABLE_ENTRY ServiceTable[] = {
+         { const_cast<LPWSTR>(L"BITSHelper"), (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+         { NULL, NULL }
+    };
+
+    StartServiceCtrlDispatcher(ServiceTable);
     return 0;
 }
