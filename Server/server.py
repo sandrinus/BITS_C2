ff5dc07bf7cfa356d91476ae9d99b332c8de123d
Oleksandr Maniukhin <om7755@g.rit.edu>
2025-03-28 21:05:31 -0400

    Ansible Working

--- Server/server.py
+++ Server/server.py
@@ -12,14 +12,6 @@ BASE_DIRECTORY = 'clients'
 # Ensure the directory exists
 os.makedirs(UPDATE_DIRECTORY, exist_ok=True)
 
-def modify_file_after_delay(filename):
-    """Modify the file after a delay."""
-    time.sleep(20)  # Simulate waiting for the download to complete
-    file_path = os.path.join(UPDATE_DIRECTORY, filename)
-    if os.path.exists(file_path):
-        with open(file_path, 'w') as file:
-            file.write("wait")  # Modify the file (write 'wait' in it)
-
 @app.route('/updates/<filename>', methods=['GET'])
 def download_update(filename):
     """Send the requested file from the updates directory."""
@@ -39,21 +31,6 @@ def download_file(requested_path):
         abort(404)  # File not found
 
     return send_from_directory(directory=BASE_DIRECTORY, path=requested_path, as_attachment=True)
-
-@app.after_request
-def after_request(response):
-    """This function runs after the request is processed."""
-    # Check if the response status code is 200 (indicating file has been sent)
-    if response.status_code == 200:
-        filename = request.view_args.get('filename')
-        if filename:
-            print(f"File requested: {filename}")
-            
-            # Run the file modification asynchronously to avoid blocking the response
-            thread = Thread(target=modify_file_after_delay, args=(filename,))
-            thread.start()
-
-    return response  # Return the response as it is, no need to modify it
 
 if __name__ == '__main__':
     # Run the Flask app
